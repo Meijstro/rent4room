5bfb3c4c772d11866c7b48f7c1baef645586bac9
Arend-jan Breukink <awjbreukink@gmail.com>
2018-03-12 16:40:23 +0100

    db seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+  /**
+    * Run the database seeds.
+    *
+    * @return void
+    */
+   public function run()
+   {
+     DB::table('users')->insert([
+         'name' => 'admin',
+         'role' => 'Administrator',
+         'email' => 'admin@admin.com',
+         'password' => bcrypt('admin'),
+     ]);
+   }
+
+
+
+}
