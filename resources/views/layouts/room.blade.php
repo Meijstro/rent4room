969836fb3069f50f58dd19af45cc9721478096e8
Arend-jan Breukink <awjbreukink@gmail.com>
2018-03-13 15:28:12 +0100

    Kamers laten zien; naam aanbieder

--- resources/views/layouts/room.blade.php
+++ resources/views/layouts/room.blade.php
@@ -2,6 +2,6 @@
 <h1>Stadnr {{$room->city_id}}, {{$room->postcode}}</h1>
 <h1>{{$room->square_meter}} m<sup>2</sup></h1>
 <h1>{{$room->price}} € p/m</h1>
-<h1>Aanbiedernr {{$room->user_id}}</h1>
+<h1>Aanbieder: {{$room->user->name}}</h1>
 <hr>
 </div>
