b0805c0d9382e300da9b8f3883894a0e976f2e25
Arend-jan Breukink <awjbreukink@gmail.com>
2018-03-15 17:30:58 +0100

    links in nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,8 +1,8 @@
 <div class="nav">
   <ul>
-    <li><a href="index.html">Home</a></li>
+    <li><a href="/">Home</a></li>
     <li><a href="about.html">Over ons</a></li>
-    <li><a href="inschrijven.html">Inschrijven</a></li>
+    <li><a href="/register">Inschrijven</a></li>
     <li><a href="storing.html">Storing melden</a></li>
     <li><a href="contact.html">Contact</a></li>
   </ul>
