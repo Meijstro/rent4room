428743c46a56c7e676aa173daefa1c94c48f63f3
Thijs Vunderink <thijsvunderink@gmail.com>
2018-03-16 15:06:01 +0100

    layout allrooms added

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- Custom styles for this template -->
-    <link href="/css/album.css" rel="stylesheet">
-  </head>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+
+        <title>Room4Rent</title>
+
+    </head>
 
   <body>
-
+    <section>
       <p>Ga je studeren in de mooiste studentenstad, schrijf je dan nu gratis in op onze website en vind de woonruimte die bij je past. </p>
           <a href="{{ route('register') }}" class="button1">Schrijf je nu in!</a>
           <p> of </p>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,37 @@
-<div class="nav">
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="about.html">Over ons</a></li>
-    <li><a href="/register">Inschrijven</a></li>
-    <li><a href="storing.html">Storing melden</a></li>
-    <li><a href="contact.html">Contact</a></li>
-  </ul>
-</div>
+<!--Navbar-->
+<nav class="navbar navbar-light navbar-1 white">
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15" aria-controls="navbarSupportedContent15"
+        aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <span><h1>Room4Rent</h1></span>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent15">
+
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Over ons</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">Inschrijven</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Storing melden</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+        <!-- Links -->
+
+    </div>
+    <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
+<hr>
