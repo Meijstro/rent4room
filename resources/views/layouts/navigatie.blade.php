636ddd652a2fc13aa973f2be8c09d89e8ae5eb13
Sjoerd Meijs <sjoerdmeijs@hotmail.com>
2018-03-27 15:49:53 +0200

    W3-14

---
+++ resources/views/layouts/navigatie.blade.php
@@ -0,0 +1,8 @@
+<div class="nav">
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">Over ons</a></li>
+    <li><a href="{{ route('register') }}">Inschrijven</a></li>
+    <li><a href="/message/0">Berichten</a></li>
+  </ul>
+</div>
