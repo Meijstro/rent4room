ec8789cf324a03339c4d9f44831f12eb9e59df37
Sjoerd Meijs <sjoerdmeijs@hotmail.com>
2018-03-27 16:31:29 +0200

    W3-5.1

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -14,17 +14,14 @@
             <li class="nav-item active">
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/about">Over ons</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">Inschrijven</a>
-            </li>
       @guest
             <li class="nav-item">
               <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
             </li>
       @else
+            <li class="nav-item">
+                <a class="nav-link" href="/message/0">Berichten</a>
+            </li>
             <li class="nav-item dropdown">
               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }} <span class="caret"></span>
