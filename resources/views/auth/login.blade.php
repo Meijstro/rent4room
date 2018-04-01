94ef373883783967f5c472dae57c0cc4ec800af4
Arend-jan Breukink <awjbreukink@gmail.com>
2018-04-01 15:13:54 +0200

    navigatie

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">@lang('phrases.email')</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -27,7 +27,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">@lang('phrases.pwd')</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -44,7 +44,7 @@
                             <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> @lang('phrases.remember')
                                     </label>
                                 </div>
                             </div>
@@ -57,7 +57,7 @@
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
+                                    @lang('phrases.forgotPwd')
                                 </a>
                             </div>
                         </div>
--- resources/views/layouts/navigatie.blade.php
+++ resources/views/layouts/navigatie.blade.php
@@ -3,5 +3,9 @@
     <li><a href="/">Home</a></li>
     <li><a href="/about">@lang('phrases.about')</a></li>
     <li><a href="{{ route('register') }}">@lang('phrases.register')</a></li>
+    <span id="taal">
+    <li><a href="/en">English</a></li>
+    <li><a href="/nl">Nederlands</a></li>
+    </span>
   </ul>
 </div>
