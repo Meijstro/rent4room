ba0134831bf44bc38d308d4a5e6aed838413bbf6
Arend-jan Breukink <awjbreukink@gmail.com>
2018-04-03 10:46:07 +0200

    navigatie

--- resources/views/layouts/navigatie.blade.php
+++ resources/views/layouts/navigatie.blade.php
@@ -3,9 +3,7 @@
     <li><a href="/">Home</a></li>
     <li><a href="/about">@lang('phrases.about')</a></li>
     <li><a href="{{ route('register') }}">@lang('phrases.register')</a></li>
-    <span id="taal">
     <li><a href="/en">English</a></li>
     <li><a href="/nl">Nederlands</a></li>
-    </span>
   </ul>
 </div>
