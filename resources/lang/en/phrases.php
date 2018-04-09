3a3b6392ac5a3223bf084d0169fd77abbf0b752b
Sjoerd Meijs <sjoerdmeijs@hotmail.com>
2018-04-09 16:03:14 +0200

    W4-14

--- resources/lang/en/phrases.php
+++ resources/lang/en/phrases.php
@@ -54,6 +54,23 @@ return [
   'send' => 'Send',
 	'intro' => 'Are you going to study in the most beautiful city in the Netherlands. <br>
 							Register now for free and find the room you that suits you most',
+	'abouttext' => '<p style font-weight="bold">Since 2018 is Room4Rent Vastgoed actief op de kamermarkt.
+								Door de hoge vraag van studentenkamers in Groningen is er besloten
+								om uitsluitend te focussen op de kamermarkt in de stad Groningen. Met een team bestaande uit drie mensen
+								wordt er dag en nacht hard gewerkt om aan de wensen van de klant te voldoen. </p>
+
+							<p>Room4Rent onderscheidt zich van andere kameraanbieders door een gerichte doelgroep te kiezen
+								en daarbij lage kosten in rekening te brengen. Wij brengen eenmalig 12,50 in rekening waarmee de klant premium wordt
+								 en zo een kamer kan verhuren of huren. </p>
+
+							<p>We blijven als bedrijf niet stilstaan en investeren in het zoeken
+								 naar potentiele huurder en verhuurders om zo een divers mogelijk
+									aanbod aan te kunnen bieden. </p>
+
+							<p>Dit beleid zetten wij voort! </p>
+
+							<p>Namens het team van Room4Rent wensen wij je veel succes bij het vinden
+								van jou ideale studentenkamer. </p>',
 
 
 ];
