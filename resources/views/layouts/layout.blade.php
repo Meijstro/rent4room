eaf1843fe874969f790cd2365ba0b3ba3bc4ac94
Arend-jan Breukink <awjbreukink@gmail.com>
2018-04-09 17:32:28 +0200

    datepicker

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,11 +7,16 @@
         <meta name="csrf-token" content="{{csrf_token()}}">
 
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <link rel="stylesheet" href="{{asset('css/style.css') }}" rel="stylesheet">
-
-        <script src="{{ asset('js/app.js') }}"></script>
+        <link href="{{asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/jquery-ui.min.css') }}" rel="stylesheet" />
+        <!-- Scripts -->
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
 
         <title>Room4Rent</title>
 
@@ -23,6 +28,13 @@
 <div class="container">
     @yield('footer')
 </div>
+<script>
+$( function() {
+  $( "#datepicker" ).datepicker({
+    dateFormat: "dd-mm-yy"
+  });
+});
+</script>
     <script async defer
     src="//maps.googleapis.com/maps/api/js?key=AIzaSyAN6TE6Vw7wPbgC9-0n2yQIrr9UmrJdVTE&callback=initMap">
       </script>
