d127e71a05a0c90e59c0d3d89f4a25ccb4a36899
Arend-jan Breukink <awjbreukink@gmail.com>
2018-04-10 09:45:14 +0200

    default rooms photos seeder

--- database/seeds/RoomsTableSeeder.php
+++ database/seeds/RoomsTableSeeder.php
@@ -90,5 +90,29 @@ class RoomsTableSeeder extends Seeder
           'date_available' => '31-03-2018',
           'description' => 'Beschrijving bij deze kamer. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.'
       ]);
+      DB::table('photos_rooms')->insert([
+          'room_id' => '1',
+          'filename' => '/storage/defaultroom.jpg',
+      ]);
+      DB::table('photos_rooms')->insert([
+          'room_id' => '2',
+          'filename' => '/storage/defaultroom.jpg',
+      ]);
+      DB::table('photos_rooms')->insert([
+          'room_id' => '3',
+          'filename' => '/storage/defaultroom.jpg',
+      ]);
+      DB::table('photos_rooms')->insert([
+          'room_id' => '4',
+          'filename' => '/storage/defaultroom.jpg',
+      ]);
+      DB::table('photos_rooms')->insert([
+          'room_id' => '5',
+          'filename' => '/storage/defaultroom.jpg',
+      ]);
+      DB::table('photos_rooms')->insert([
+          'room_id' => '6',
+          'filename' => '/storage/defaultroom.jpg',
+      ]);
     }
 }
